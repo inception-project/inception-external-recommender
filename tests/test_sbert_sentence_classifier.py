7ad25a79e7884d83fea6165aaf3e51063525908e
Jan-Christoph Klie <git@mrklie.com>
2020-12-01 16:39:31 +0100

    #7 - Add Adapter Based Recommender
    - Mark slow tests

--- tests/test_sbert_sentence_classifier.py
+++ tests/test_sbert_sentence_classifier.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
+import pytest
+
 from ariadne.contrib import SbertSentenceClassifier
 
 from tests.util import *
 
 
+@pytest.mark.skip(reason="Slow")
 def test_fit(tmpdir_factory):
     training_data = load_newsgroup_training_data()
 
@@ -16,6 +19,7 @@ def test_fit(tmpdir_factory):
     assert model_path.is_file(), f"Expected {model_path} to be a file!"
 
 
+@pytest.mark.skip(reason="Slow")
 def test_predict(tmpdir_factory):
     training_data = load_newsgroup_training_data()
     model_directory = Path(tmpdir_factory.mktemp("models"))
