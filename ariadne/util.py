820a729945731cac0c0dbdedb0617a5d3d043aa7
Pascal Gula <pascal.gula@gmail.com>
2021-11-04 14:55:00 +0100

    Update util.py
    Removes unused Path and wget. Adds level as param with DEBUG default value.

--- ariadne/util.py
+++ ariadne/util.py
@@ -1,12 +1,9 @@
 import logging
-from pathlib import Path
-
-import wget
 
 
-def setup_logging():
+def setup_logging(level=logging.DEBUG):
     log_fmt = "%(process)d-%(thread)d %(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.DEBUG, format=log_fmt)
+    logging.basicConfig(level=level, format=log_fmt)
 
     filelock_logger = logging.getLogger("filelock")
     filelock_logger.setLevel(logging.WARNING)
